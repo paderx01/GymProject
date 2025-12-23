e89446b3f3d0323271a5d77109514fa104e24ac3
Pader <paderx26@gmail.com>
2025-12-23 17:27:36 -0600

    added more components

---
+++ src/components/HeroBanner.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Box, Stack, Typography } from '@mui/material'
+import HeroBannerImage from '../assets/images/banner.png'
+
+const HeroBanner = () => {
+  return (
+    <Box>
+        <Typography color ='#FF2625' fontWeight='600' fontSize = > 
+            Fitness Club
+        </Typography>
+    </Box>
+  )
+}
+
+export default HeroBanner
