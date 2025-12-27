a748b181d7563234b498f61e885804ec03ec8baf
Pader <paderx26@gmail.com>
2025-12-27 12:59:21 -0600

    updated hero banner

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -12,17 +12,35 @@ const HeroBanner = () => {
             Fitness Club
         </Typography>
         <Typography fontWeight ={700}
-        sx = {{ fontSize: { lg: '44px', xs: '40px'}}}>
+        sx = {{ fontSize: { lg: '44px', xs: '40px'}}}
+        mb="23px"
+        mt="30px">
             Sweat, Smile <br/> and Repeat
         </Typography >
-         <Typography fontSize = "22px" lineHeight = "35px" mb={3}>
+         <Typography fontSize = "22px" lineHeight = "35px" mb={4}>
             Check out the most effective exercises
         </Typography>
-        <Button variant = "contained" color = "error" href="#exercises">
+        <Button variant = "contained" color = "error" href="#exercises"
+         sx ={{background: '#ff2625' , 
+            padding: ' 10px'
+         }}
+        >
             Explore Exercises
         </Button>
+        <Typography
+            fontWeight = {600}
+            color="#ff2625"
+            sx={{
+                opacity: 0.1,
+                display: {lg: 'block', xs: 'none'}
+            }}
+            fontSize = "200px"
+            >
+            Exercise
+        </Typography>
         <img src ={HeroBannerImage} alt="banner"
-    />
+        className='hero-banner-img'
+        />
     </Box>
   )
 }
