777e53437ea2e46d02cc9926e6dd24c81c342599
Pader <paderx26@gmail.com>
2025-12-28 17:45:15 -0600

    coding SearchExercise component

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,8 +1,27 @@
-import React from "react"
+import React, { useEffect, useState } from 'react';
+import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 
 const SearchExercises = () =>{
     return(
-        <div>SearchExercises</div>
+        <Stack alignItems="center"
+         mt ="37px" justifyContent="center"
+         p='20px'>
+        <Typography fontWeight = {700} xs={{
+            fontSize: { lg: '44px', xs: '30px' }
+        }} mb='50px' textAlign="center">
+            Awesome Exercise You <br/>
+            Should Know 
+        </Typography>
+        <Box position="relative"  mb="72 px">
+            <Textfield
+                height=" 76px "
+                value=" "
+                onChange={(e) => {}}
+                placeHolder = "Search Exercises"
+                type = "text"
+            />
+        </Box>
+        </Stack> 
     )
 }
 
