677371f3056e0fc5b5dd505ba85979c055ce55de
Pader <paderx26@gmail.com>
2026-01-03 13:53:17 -0600

    added exercise component

---
+++ src/components/ExerciseCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Box, Stack, Typography} from '@mui/material'
+
+const ExerciseCard  = () => {
+  return (
+    <div> Exercise Card</div>
+  ) 
+}
+
+export default  ExerciseCard
--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
 import { Box, Typography } from '@mui/material';
 
@@ -6,7 +6,7 @@ import ExerciseCard from './ExerciseCard';
 import BodyPart from './BodyPart';
 import RightArrowIcon from '../assets/icons/right-arrow.png';
 import LeftArrowIcon from '../assets/icons/left-arrow.png';
- 
+
 const LeftArrow = () => {
   const { scrollPrev } = useContext(VisibilityContext);
 
