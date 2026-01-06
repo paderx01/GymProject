9643559c6d1de7a3e480ff4dde2b1a958563a42f
Pader <paderx26@gmail.com>
2026-01-06 17:36:34 -0600

    finished pagination with exercises bodyparts

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -32,11 +32,10 @@ const HorizontalScrollbar = ({ data, bodyParts, setBodyPart, bodyPart }) => (
     {data.map((item) => (
       <Box
         key={item.id || item}
-        itemID={item.id || item}
+        itemId={item.id || item}
         title={item.id || item}
         m="0 40px"
       >
-  
         {bodyParts ? <BodyPart item={item} setBodyPart={setBodyPart} bodyPart={bodyPart} /> : <ExerciseCard exercise={item} /> }
       </Box>
     ))}
