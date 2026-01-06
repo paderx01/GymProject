996c9f5d720af196eff1b7f7bbcba01dba4e7d13
Pader <paderx26@gmail.com>
2026-01-06 13:33:18 -0600

    exercises, only showing 9 exercises at a time

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -8,9 +8,17 @@ const Exercises = ({ exercises, setExercises,  bodyPart }) => {
  const [currentPage, setCurrentPage] = useState(1)
  const exercisesPerPage = 9; 
 
- const paginate  = (e, value) => {}
+ const indexOfLastExercise = currentPage * exercisesPerPage;
+ const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage
 
-  return 
+const currentExercises = exercises.slice(indexOfFirstExercise, indexOfLastExercise)
+
+ const paginate  = (e, value) => {
+  setCurrentPage(value);
+  window.scrollTo({top: 1800, behavior: 'smooth'})
+ }
+
+  return  
     <Box id="exercises"
       sx = {{ mt: {lg:'110px'  }}}
       mt = '50px' 
@@ -22,7 +30,7 @@ const Exercises = ({ exercises, setExercises,  bodyPart }) => {
       <Stack direction = "row" xs = {{
           gap: {lg: '110px', xs: '50px ' }
       }} flexWrap =  'wrap' justifyContent = 'center '> 
-         {exercises.map((exercise, index) => (
+         {currentExercises.map((exercise, index) => (
           <ExerciseCard key={index} exercise = {exercise} />
          ))}
       </Stack> 
