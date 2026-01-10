58e8ca792fe94a9075f5f8849754f3c057b322eb
Pader <paderx26@gmail.com>
2026-01-09 18:34:24 -0600

    adding exercise details

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,17 +1,34 @@
 import React, { useEffect, useState } from 'react'
 import { useParams} from 'react-router-dom'
-import { Box } from '@mui/icons-material'
+import { Box } from '@mui/material'
 
 import { exerciseOptions, fetchData } from '../utils/fetchData';
+import Detail from '../components/Detail';
+import ExerciseVideo from '../components/ExerciseVideo';
+import SimilarExercises from '../components/SimilarExercises';
 
 const ExerciseDetail = () => {
-  return (
+  useState [exerciseDetail, setExerciseDetail] = 
+  useState({});
+  const { id } = useparams(); 
+  
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com';
+      const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com'
+
+      const exerciseDetailData = await fetchData(`${excerciseDbUrl}/exercise/exercise{id}` , exerciseOptions)
+      setExerciseDetail(exerciseDetailData)
+    }
+  }, [id]);
+  fetchExercisesData();
+  return(
     <Box>
-      <Detail />
+      <Detail exerciseDetail={exerciseDetail} />
       <ExerciseVideo />
       <SimilarExercises />
     </Box>
-  )
-}
+  );
+};
 
 export default ExerciseDetail
